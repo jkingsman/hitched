f8a90882238299df902a7bf4e8b496cfd5b71c02
jkingsman <jack.kingsman@gmail.com>
2018-02-27 07:42:53 -0800

    update gunicorn launch

--- app/launch_gunicorn.sh
+++ app/launch_gunicorn.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-NAME="core.wsgi"                                  # Name of the application
-DJANGODIR=/home/ubuntu/hitched/app             # Django project directory
-SOCKFILE=/home/ubuntu/hitched/gunicorn.sock  # we will communicte using this unix socket
-USER=ubuntu                                        # the user to run as
-GROUP=ubuntu                                     # the group to run as
-NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=settings_prod           # which settings file should Django use
-DJANGO_WSGI_MODULE=core.wsgi                     # WSGI module name
+NAME="hitched"                              #Name of the application (*)
+DJANGODIR=/srv/hitched/app/core          # Django project directory (*)
+SOCKFILE=/srv/hitched/app/gunicornsock        # we will communicate using this unix socket (*)
+USER=ec2-user                                        # the user to run as (*)
+GROUP=ec2-user                                   # the group to run as (*)
+NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
+DJANGO_SETTINGS_MODULE=core.settings_prod         # which settings file should Django use (*)
+DJANGO_WSGI_MODULE=core.wsgi                     # WSGI module name (*)
 
 echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -17,10 +20,8 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-gunicorn ${DJANGO_WSGI_MODULE} \
+exec /usr/local/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP \
-  --bind=unix:$SOCKFILE \
-  --log-level=debug \
-  --log-file=/tmp/gunicorn.log
+  --user $USER \
+  --bind=unix:$SOCKFILE

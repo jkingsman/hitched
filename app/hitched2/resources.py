12a436b4cf29a7c17e529fe4b740fd1ed8db5106
Jack Kingsman <jack.kingsman@gmail.com>
2018-02-27 03:19:22 -0800

    Readd lost dires

---
+++ app/hitched2/resources.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers, viewsets
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
---
+++ app/hitched2/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from rest_framework.authtoken import views
+
+from hitched2.views.logout import Logout
+
+urlpatterns = [
+    url(r'^', include('invitee.urls')),
+    url(r'^', include('message.urls')),
+
+    url(r'^get-token/', views.obtain_auth_token),
+    url(r'^discard-token/', Logout.as_view()),
+]

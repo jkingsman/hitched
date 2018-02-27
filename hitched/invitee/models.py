1a07e7b18f5a0e60aa9cbdc9629e895f1f6629c1
Jack Kingsman <jack.kingsman@gmail.com>
2018-02-27 02:53:41 -0800

    Remove old scraps

--- hitched/invitee/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Invitee(models.Model):
-    name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=13)
-    status = models.CharField(max_length=200)

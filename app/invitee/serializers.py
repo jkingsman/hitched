aa52dd85a2eb945ff6d14702547432fbda50d8e5
jkingsman <jack.kingsman@gmail.com>
2018-02-27 11:05:14 -0800

    Halfway through status change tracking

--- app/invitee/serializers.py
+++ app/invitee/serializers.py
@@ -3,6 +3,15 @@ from invitee.models import Invitee
 
 
 class InviteeSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.status = validated_data.get('status', instance.status)
+        instance.created = validated_data.get('created', instance.created)
+
+        instance.save()
+        return instance
+
     class Meta:
         model = Invitee
         fields = ('id', 'name', 'phone', 'status')

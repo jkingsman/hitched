9c3b53ca0d8d61ee973cd2d7f2e08c46e0302245
jkingsman <jack.kingsman@gmail.com>
2018-03-03 10:59:20 -0800

    Improve

--- app/launch_gunicorn.sh
+++ app/launch_gunicorn.sh
@@ -1,19 +1,10 @@
-#!/bin/bash
-
-SOCKFILE=/srv/hitched/app/gunicornsock        # we will communicate using this unix socket (*)
-
-# Activate the virtual environment
-cd $DJANGODIR
-
-
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 export DJANGO_SETTINGS_MODULE="core.settings_prod"
 
-
-gunicorn core.wsgi :application \
+gunicorn core.wsgi:application \
   --name hitched \
   --workers 1 \
   --user $USER \
-  --bind=unix:$SOCKFILE \
+  --bind=unix:/srv/hitched/app/gunicornsock \
   --daemon

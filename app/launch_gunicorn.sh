4267e72900c95e7b0946b7ff5b0354a5a390c090
jkingsman <jack.kingsman@gmail.com>
2018-03-03 10:57:34 -0800

    UPdate gunicorn conf

--- app/launch_gunicorn.sh
+++ app/launch_gunicorn.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-NAME="hitched"                              #Name of the application (*)
-DJANGODIR=/srv/hitched/app          # Django project directory (*)
 SOCKFILE=/srv/hitched/app/gunicornsock        # we will communicate using this unix socket (*)
-USER=ec2-user                                        # the user to run as (*)
-GROUP=ec2-user                                   # the group to run as (*)
-NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
-DJANGO_SETTINGS_MODULE=core.settings_prod         # which settings file should Django use (*)
-DJANGO_WSGI_MODULE=core.wsgi                     # WSGI module name (*)
-
-echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
 
-# Create the run directory if it doesn't exist
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 export DJANGO_SETTINGS_MODULE="core.settings_prod"
 
 while true;
-do exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME \
-  --workers $NUM_WORKERS \
+
+do exec gunicorn core.wsgi :application \
+  --name hitched \
+  --workers 1 \
   --user $USER \
   --bind=unix:$SOCKFILE && break;
+  --daemon
 done
